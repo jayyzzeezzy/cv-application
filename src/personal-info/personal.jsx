37a90fa5abc9c34b92d8912185cd911cc71496f2
James Zhou <jam9es@gmail.com>
2024-08-29 17:27:07 -0700

    Include state variables for phone, city, and email

--- src/personal-info/personal.jsx
+++ src/personal-info/personal.jsx
@@ -2,14 +2,32 @@ import { useState } from "react";
 
 function Personal() {
     const [fullName, setFullName] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [email, setEmail] = useState('');
+    const [city, setCity] = useState('');
 
     function handleNameChange(e) {
         setFullName(e.target.value);
     }
 
+    function handlePhoneChange(e) {
+        setPhoneNumber(e.target.value);
+    }
+
+    function handleEmailChange(e) {
+        setEmail(e.target.value);
+    }
+
+    function handleCityChange(e) {
+        setCity(e.target.value);
+    }
+
     function handleSubmit(e) {
         e.preventDefault();
         console.log(fullName);
+        console.log(phoneNumber);
+        console.log(email);
+        console.log(city);
     };
 
 
@@ -18,7 +36,7 @@ function Personal() {
             <form onSubmit={handleSubmit}>
                 <legend>Personal Information</legend>
                 <div className="input">
-                    <label htmlFor="full-name" className="full-name">Full Name</label>
+                    <label htmlFor="full-name">Full Name</label>
                     <input 
                         id="full-name" 
                         type="text" 
@@ -30,17 +48,35 @@ function Personal() {
 
                 <div className="input">
                     <label htmlFor="tel">Phone Number</label>
-                    <input id="tel" type="tel" name="tel"/>
+                    <input 
+                        id="tel" 
+                        type="tel" 
+                        name="tel"
+                        value={phoneNumber}
+                        onChange={handlePhoneChange}
+                    />
                 </div>
                 
                 <div className="input">
                     <label htmlFor="email">Email</label>
-                    <input id="email" type="email" name="email" />
+                    <input 
+                        id="email" 
+                        type="email" 
+                        name="email" 
+                        value={email}
+                        onChange={handleEmailChange}
+                    />
                 </div>
 
                 <div className="input">
                     <label htmlFor="City">City</label>
-                    <input id="city" type="text" name="city" />
+                    <input 
+                        id="city" 
+                        type="text" 
+                        name="city" 
+                        value={city}
+                        onChange={handleCityChange}
+                    />
                 </div>
 
                 <div className="submit">
